03edd6f95cadca816c5d18153cc2dd93834000c8
Stanislaw Nagiec <>
2018-04-26 11:44:23 -0400

    tons of refactoring, added ability to appreciate multiple users and each will be invited to channel

--- natbot/appreciate.py
+++ natbot/appreciate.py
@@ -13,7 +13,7 @@ import logging
 logger = logging.getLogger('natbot')
 logger.addHandler(logging.FileHandler('natbot.log'))
 logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 application = Flask(__name__)
 
@@ -25,43 +25,40 @@ application = Flask(__name__)
 @application.route('/APR', methods=['POST'])
 def hello():
     logger.info("===================================================================")
-    accepted_format = "Accepted format: /appreciate [@user] [message]"
+    accepted_format = "Accepted format: /appreciate [@user(s)] [message]"
     origin_url = request.form['response_url']
     submitting_user_id = request.form['user_id']
     submitting_user_name = request.form['user_name']
     logger.info("Appreciation submitted by " + submitting_user_id + " aka " + submitting_user_name)
 
-    original_message = (request.form['text'])
-    entire_message = original_message.replace("[", "").replace("]", "")
+    raw_message = (request.form['text'])
+    entire_message = raw_message.replace("[", "").replace("]", "")
     logger.info("INFO: entire_message = " + entire_message)
 
     if entire_message.lower() == "help":
         logger.info("INFO: Help requested")
         stas_user_id = "U"
-        return("Need help? Sure thing! Try the following to correctly format your message. \n" + accepted_format + "\n If you help with something else, or have feedback, slack <@" + stas_user_id + "> directly.")
+        return("Need help? Try the following to correctly format your message. \n" + accepted_format + "\n If you help with something else, or have feedback, slack <@" + stas_user_id + "> directly.")
 
-    try:
-        receiving_user, appreciation_message = entire_message.split(" ", 1)
-    except:
-        logger.error("ERROR: Could not split entire_message")
-        return("Hmm, your message doesn't appear to be formatted correctly. Try the following. \n" + accepted_format)
+    receiving_users = []
+    rgx = r'<@([\d\w]+?)\|(.+?)>'
+    import re
+    matches = re.finditer(rgx, entire_message)
+    stripped_message = entire_message
+    for m in matches:
+        user_id = m.group(1)
+        receiving_users.append(user_id)
+        stripped_message = stripped_message.replace(m.group(2), "x")
+    logger.info("INFO: receiving_users = %s" % receiving_users)
 
-    receiving_user_id_formatted = receiving_user.split("|")[0] + ">"
-    if not receiving_user_id_formatted.startswith("<@"):
-        logger.error("ERROR: Message did not begin with a receiving_user")
+    if not receiving_users:
+        logger.error("ERROR: Message did not contain a receiving_user")
         return("Hmm, slack could not find a valid user to address your appreciation to. Double check your message formatting. \n" + accepted_format)
-    receiving_user_id = receiving_user_id_formatted.replace("<", "").replace("@", "").replace(">", "")
 
-    # import string
-    # stripped_message = ''.join(st for st in appreciation_message if st not in string.punctuation and not st == ' ')
-    stripped_message = appreciation_message    # delete this line if you uncomment the above two
     logger.info("INFO: stripped_message = " + stripped_message)
-
     if profanity.contains_profanity(stripped_message):
         logger.info("UH-OH: We've got ourselves a potty mouth.")
         return("NO SWEARING!")
-    logger.info("Appreciation to be recieved by " + receiving_user)
-    logger.info(appreciation_message)
 
     watercooler_url = "https://hooks.slack.com/services/url"
     watercooler_id = "C" # <#C|watercooler>
@@ -72,20 +69,22 @@ def hello():
     debug_auth_token = "xoxp-token"
     natbot_auth_token = "xoxp-token"
 
-    def do_post(origin_url, receiving_user, appreciation_message):
+    def do_post(origin_url, receiving_users, entire_message):
         import time
         time.sleep(1)
-        r_post_watercooler = requests.post(watercooler_url, json = {"text" : receiving_user + " " + appreciation_message, "response_type" : "in_channel"})
-        r_post_appreciate = requests.post(appreciate_url, json = {"text" : receiving_user + " " + appreciation_message, "response_type" : "in_channel"})
-        r_invite_watercooler = requests.post(invite_url, headers={'Authorization' : 'Bearer ' + natbot_auth_token, 'Content-type' : 'application/json; charset=utf-8'}, json = {"channel" : watercooler_id, "user" : receiving_user_id})
-        r_invite_appreciate = requests.post(invite_url, headers={'Authorization' : 'Bearer ' + natbot_auth_token, 'Content-type' : 'application/json; charset=utf-8'}, json = {"channel" : appreciate_id, "user" : receiving_user_id})
-        logger.info("requests.post() message to watercooler returns: " + r_post_watercooler.text)
-        logger.info("requests.post() message to appreciate returns: " + r_post_appreciate.text)
-        logger.info("requests.post() invite " + receiving_user + " to watercooler returns: " + r_invite_watercooler.text)
-        logger.info("requests.post() invite " + receiving_user + " to appreciate returns: " + r_invite_appreciate.text)
+        r_post_watercooler = requests.post(watercooler_url, json = {"text" : entire_message, "response_type" : "in_channel"})
+        r_post_appreciate = requests.post(appreciate_url, json = {"text" : entire_message, "response_type" : "in_channel"})
+        logger.info("INFO: requests.post() message to watercooler returns: " + r_post_watercooler.text)
+        logger.info("INFO: requests.post() message to appreciate returns: " + r_post_appreciate.text)
+
+        for user in receiving_users:
+            r_invite_watercooler = requests.post(invite_url, headers={'Authorization' : 'Bearer ' + natbot_auth_token, 'Content-type' : 'application/json; charset=utf-8'}, json = {"channel" : watercooler_id, "user" : user})
+            r_invite_appreciate = requests.post(invite_url, headers={'Authorization' : 'Bearer ' + natbot_auth_token, 'Content-type' : 'application/json; charset=utf-8'}, json = {"channel" : appreciate_id, "user" : user})
+            logger.info("INFO: requests.post() invite " + user + " to watercooler returns: " + r_invite_watercooler.text)
+            logger.info("INFO: requests.post() invite " + user + " to appreciate returns: " + r_invite_appreciate.text)
 
     from threading import Thread
-    thread = Thread(target=do_post, kwargs={'origin_url': origin_url, 'receiving_user': receiving_user, 'appreciation_message': appreciation_message})
+    thread = Thread(target=do_post, kwargs={'origin_url': origin_url, 'receiving_user': receiving_user, 'entire_message': entire_message})
     thread.start()
     return ""
 
